eebebfc7e6a4645aa1915dc6e1952ed03f308614
Sandy Nguyen <69399429+sandyngu@users.noreply.github.com>
2020-10-16 12:11:02 -0400

    Routes and Imports Done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Card from './components/Card/Card';
+import Page1 from './components/Page1/Page1';
+// import Page2 from './components/Page2/Page2';
+// import Page3 from './components/Page3/Page3';
+// import Page4 from './components/Page4/Page4';
+// import Page5 from './components/Page5/Page5';
+// import Page6 from './components/Page6/Page6';
 import './App.css';
 
 function App() {
@@ -9,11 +16,11 @@ function App() {
         <Switch>
           <Route path='/' component={Card}/>
           <Route path='/1' component={Page1}/>
-          <Route path='/2' component={Page2}/>
+          {/* <Route path='/2' component={Page2}/>
           <Route path='/3' component={Page3}/>
           <Route path='/4' component={Page4}/>
           <Route path='/5' component={Page5}/>
-          <Route path='/6' component={Page6}/>
+          <Route path='/6' component={Page6}/> */}
         </Switch>
       </div>
     </BrowserRouter>
---
+++ src/components/Page1/Page1.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import page1 from '../../assets/icons/page1.png';
+
+function Page1() {
+    return (
+        <div className="page1">
+            <img src={page1} alt="Page 1" className="page1__screen"/>
+        </div>
+    );
+};
+
+export default Page1;
