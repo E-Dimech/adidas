577b387716567c26f65731ad6cdfd4ce238c0005
Sandy Nguyen <69399429+sandyngu@users.noreply.github.com>
2020-10-16 12:04:36 -0400

    Routes Made

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import Modal from './components/Modal/Modal';
-import PrimeBlueModal from './components/PrimeBlue/PrimeBlue';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      {/* <Modal /> */}
-      {/* <PrimeBlueModal /> */}
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route path='/' component={Card}/>
+          <Route path='/1' component={Page1}/>
+          <Route path='/2' component={Page2}/>
+          <Route path='/3' component={Page3}/>
+          <Route path='/4' component={Page4}/>
+          <Route path='/5' component={Page5}/>
+          <Route path='/6' component={Page6}/>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
