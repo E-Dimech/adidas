6f838a26c9678e0617b5c7d246737c70a6ffd1a5
Eric Dimech <ericdimech@hotmail.com>
2020-10-16 12:04:28 -0400

    Updated merge

---
+++ src/components/Card/Card.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Modal from "./Modal/Modal";
+import Leaf from "../../assets/icons/leaf.png";
+import Info from "../../assets/icons/info.svg";
+import White from "../../assets/shoes/WhiteShoe.png";
+
+import "./Card.scss";
+
+class Card extends React.Component {
+  state = {
+    modalDisplay: false,
+  };
+
+  activateModal = () => {
+    this.setState({
+      modalDisplay: true,
+    });
+  };
+
+  closeModal = () => {
+    this.setState({
+      modalDisplay: false,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <div className="card-display">
+          <div className="card">
+            <div className="card__images">
+              <img
+                className="card__shoe-page"
+                src={White}
+                alt="sustainable shoe"
+              />
+              <img className="card__leaf-white" src={Leaf} alt="leaf" />
+              <img
+                className="card__info-white"
+                onClick={() => this.activateModal}
+                src={Info}
+                alt="information"
+              />
+            </div>
+          </div>
+        </div>
+        <Modal
+          modalDisplay={this.state.modalDisplay}
+          closeModal={this.closeModal}
+        />
+      </>
+    );
+  }
+}
+
+export default Card;
