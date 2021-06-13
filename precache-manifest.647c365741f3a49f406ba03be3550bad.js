5625f565592a615b8ae3402737c192498cae0d73
Sandy Nguyen <69399429+sandyngu@users.noreply.github.com>
2021-06-13 11:37:23 -0400

    Updates

---
+++ precache-manifest.647c365741f3a49f406ba03be3550bad.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a28479f547a57dd03e0d8f0a189cdd5",
+    "url": "/4d-hackathon-adidas/index.html"
+  },
+  {
+    "revision": "3a3891eaf1823ef10862",
+    "url": "/4d-hackathon-adidas/static/css/main.9972bb99.chunk.css"
+  },
+  {
+    "revision": "2b8baeb9528bc3d1921d",
+    "url": "/4d-hackathon-adidas/static/js/2.6ecb3285.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/4d-hackathon-adidas/static/js/2.6ecb3285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a3891eaf1823ef10862",
+    "url": "/4d-hackathon-adidas/static/js/main.69cc5a1b.chunk.js"
+  },
+  {
+    "revision": "34fca4c63574b8343b23",
+    "url": "/4d-hackathon-adidas/static/js/runtime-main.a3370d72.js"
+  },
+  {
+    "revision": "c44f8750cb24799348617c8ab18d4adb",
+    "url": "/4d-hackathon-adidas/static/media/close.c44f8750.svg"
+  },
+  {
+    "revision": "a97515b0afc9780930a8a108817b1c2b",
+    "url": "/4d-hackathon-adidas/static/media/info.a97515b0.svg"
+  },
+  {
+    "revision": "00a28b60aad9a245e26a145e0518be0b",
+    "url": "/4d-hackathon-adidas/static/media/leaf.00a28b60.png"
+  },
+  {
+    "revision": "d932c2dc37d1656ec258ec678559797d",
+    "url": "/4d-hackathon-adidas/static/media/page1.d932c2dc.png"
+  },
+  {
+    "revision": "175aea1038f3d9ee9411bbf9040e2b44",
+    "url": "/4d-hackathon-adidas/static/media/page2.175aea10.png"
+  },
+  {
+    "revision": "ee764ff010b4ac5c98e92ae10f6c1db6",
+    "url": "/4d-hackathon-adidas/static/media/page3.ee764ff0.png"
+  },
+  {
+    "revision": "1a560ac72ed352878f497fb591e99d62",
+    "url": "/4d-hackathon-adidas/static/media/page4.1a560ac7.png"
+  },
+  {
+    "revision": "7020236df1e5b98f17cd51fe85128f04",
+    "url": "/4d-hackathon-adidas/static/media/page5.7020236d.png"
+  },
+  {
+    "revision": "18931ec8a1bf0f5c035fb1f1c7c3a8a9",
+    "url": "/4d-hackathon-adidas/static/media/page6.18931ec8.png"
+  }
+]);
